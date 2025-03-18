77c694d840f1fdd9acbe85cb874e6f6852e9b6d2
cosmeak <90453342+Cosmeak@users.noreply.github.com>
2025-03-18 17:35:38 +0100

    feat: rework configuration

--- flake.nix
+++ flake.nix
@@ -13,72 +13,10 @@
       modules = [
         # SD card image
         "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-
         # Raspberry Pi 3 hardware modules
-        hardware.nixosModules.raspberry-pi-3 
-
-        # Configuration
-        ({ self, inputs, pkgs, lib, ... }: { config = { 
-          # Used to build image/version
-          nixpkgs.buildPlatform.system = "x86_64-linux";
-          nixpkgs.hostPlatform.system = "aarch64-linux";
-          nixpkgs.config.allowUnsupportedSystem = true;
-
-          # Networking configuration
-          networking.hostName = "rpi";
-          networking.interfaces."wlan0".useDHCP = true;
-          networking.wireless = {
-            enable = true;
-            interfaces = [ "wlan0" ];
-          };
-
-          # Window Manager (see https://wiki.nixos.org/wiki/Dwm & https://dwm.suckless.org/)
-          services.xserver.windowManager.dwm.enable = true;
-
-          # System wide packages (see https://search.nixos.org/packages)
-          environment.systemPackages  = with pkgs; [
-            firefox
-          ];
-
-          # Users configuration
-          users.users."rpi" = {
-            isNormalUser = true;
-            # wheel = admin
-            extraGroups = [ "wheel" ]; 
-            # password generated with `mkpasswd` command
-            hashedPassword = "$y$j9T$rg0syrPPtjaLILPTTplI3/$5uykqP9tXjAsvOocbfosUeN6j6dMrHRUtwudKd4QaA5"; 
-
-            # User wide packages
-            packages = [];
-          };
-
-          # Perform garbage collection weekly to maintain low disk usage
-          nix.gc = {
-            automatic = true;
-            dates = "weekly";
-            options = "--delete-older-than 1w";
-          };
-
-          # Optimize storage
-          # You can also manually optimize the store via:
-          #    nix-store --optimise
-          # Refer to the following link for more details:
-          # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
-          nix.settings.auto-optimise-store = true;
-
-          # TODO: auto update flake.lock with reboot
-
-          # TODO: wake-on-lan at 9:00 + auto shutdown at 18:00
-          # wake up TV with it and shut it down too
-
-          # This value determines the NixOS release from which the default
-          # settings for stateful data, like file locations and database versions
-          # on your system were taken. It‘s perfectly fine and recommended to leave
-          # this value at the release version of the first install of this system.
-          # Before changing this value read the documentation for this option
-          # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-          system.stateVersion = "24.11";
-        };})
+        hardware.nixosModules.raspberry-pi-3
+        # Configuration module
+        ./nixos/rpi.nix
       ];
     };
   };
---
+++ nixos/rpi.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, ... }:
+{
+  # Allow licensed firmware to be update
+  hardware.enableRedistributableFirmware = true;
+
+  # Early boot
+  boot.loader.raspberryPi.enable = true;
+  boot.loader.version = 3;
+  boot.loader.firmwareConfig = ''
+    core_freq=250
+  '';
+  boot.initrd.kernelModules = [ "vc4" "bcm2835_dma" "i2c_bcm2835" ];
+
+  # Network
+  networking.hostName = "rpi";
+  networking.wireless.enable = true;
+  networking.wireless.interfaces = [ "wlan0" ];
+  networking.interfaces."wlan0".useDHCP = true;
+
+  # Time
+  time.timezone = "Europe/Paris";
+
+  # Keyboard
+  services.xserver.xkb.layout = "fr,us";
+  services.xserver.xkb.xkbOptions = "grp:win_space_toggle";
+
+  # Desktop
+  services.xserver.enable = true;
+  services.xserver.desktopManager.lxqt.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+
+  # Environment wide packages
+  environment.systemPackages = with pkgs; [
+    libraspberrypi
+    git
+    vim
+    nano
+    firefox
+    feet
+  ];
+
+  # TODO: auto update flake.lock with reboot
+
+  # TODO: wake-on-lan at 9:00 + auto shutdown at 18:00
+  # wake up TV with it and shut it down too
+
+  # Configure users
+  users.mutableUsers = false;
+  users.users.rpi = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ]; # wheel = admin
+    # password generated with `mkpasswd` command
+    hashedPassword = "$y$j9T$rg0syrPPtjaLILPTTplI3/$5uykqP9tXjAsvOocbfosUeN6j6dMrHRUtwudKd4QaA5";
+  };
+
+  # Nix settings
+  nix.settings.experimental-features = "nix-command flakes";
+  nix.settings.trusted-users = [ "root" "@wheel" ];
+
+  # Garbage collection
+  nix.gc.automatic = true;
+  nix.gc.dates = "weekly";
+  nix.gc.options = "--delete-older-than 1w";
+  nix.settings.auto-optimise-store = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11";
+}
