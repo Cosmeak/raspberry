cb8f105a7340778fc85a604ee587e8c6b8d575b7
cosmeak <guillaume.fine@cosmeak.dev>
2025-03-18 09:01:34 +0000

    feat: basic image

--- flake.nix
+++ flake.nix
@@ -1,109 +1,85 @@
 {
-    description = "Magnetis Rasberry Pi services";
+  description = "NixOS Raspberry Pi configuration flake";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-        hardware.url = "github:NixOS/nixos-hardware/master";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+
+  outputs = { self, nixpkgs, hardware, ... }@inputs: {
+    # Raspberry configuration
+    nixosConfigurations.rpi = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        # SD card image
+        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+
+        # Raspberry Pi 3 hardware modules
+        hardware.nixosModules.raspberry-pi-3 
+
+        # Configuration
+        ({ self, inputs, pkgs, lib, ... }: { config = { 
+          # Used to build image/version
+          nixpkgs.buildPlatform.system = "x86_64-linux";
+          nixpkgs.hostPlatform.system = "aarch64-linux";
+          nixpkgs.config.allowUnsupportedSystem = true;
+
+          # Networking configuration
+          networking.hostName = "rpi";
+          networking.interfaces."wlan0".useDHCP = true;
+          networking.wireless = {
+            enable = true;
+            interfaces = [ "wlan0" ];
+          };
+
+          # Window Manager (see https://wiki.nixos.org/wiki/Dwm & https://dwm.suckless.org/)
+          services.xserver.windowManager.dwm.enable = true;
+
+          # System wide packages (see https://search.nixos.org/packages)
+          environment.systemPackages  = with pkgs; [
+            firefox
+          ];
+
+          # Users configuration
+          users.users."rpi" = {
+            isNormalUser = true;
+            # wheel = admin
+            extraGroups = [ "wheel" ]; 
+            # password generated with `mkpasswd` command
+            hashedPassword = "$y$j9T$rg0syrPPtjaLILPTTplI3/$5uykqP9tXjAsvOocbfosUeN6j6dMrHRUtwudKd4QaA5"; 
+
+            # User wide packages
+            packages = [];
+          };
+
+          # Perform garbage collection weekly to maintain low disk usage
+          nix.gc = {
+            automatic = true;
+            dates = "weekly";
+            options = "--delete-older-than 1w";
+          };
+
+          # Optimize storage
+          # You can also manually optimize the store via:
+          #    nix-store --optimise
+          # Refer to the following link for more details:
+          # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+          nix.settings.auto-optimise-store = true;
+
+          # TODO: auto update flake.lock with reboot
+
+          # TODO: wake-on-lan at 9:00 + auto shutdown at 18:00
+          # wake up TV with it and shut it down too
+
+          # This value determines the NixOS release from which the default
+          # settings for stateful data, like file locations and database versions
+          # on your system were taken. It‘s perfectly fine and recommended to leave
+          # this value at the release version of the first install of this system.
+          # Before changing this value read the documentation for this option
+          # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+          system.stateVersion = "24.11";
+        };})
+      ];
     };
-
-    outputs = { self, nixpkgs, hardware, ... }@inputs: 
-    {
-        # Raspberry Pi 4 configuration
-        nixosConfiguration."rpi" = nixpkgs.lib.nixosSystem {
-            system = "aarch64-linux";
-
-            modules = [ 
-                hardware.nixosModules.rapsberry-pi-4 
-                "${nixpkgs}/nixos/modules/profiles/minimal.nix"
-                {
-                    # Force update to the last linux kernel avalaible
-                    boot.kernelPackages = nixpkgs.linuxPackages_latest;
-
-                    # Special hardware config for raspberry
-                    hardware = {
-                        raspberry-pi."4".apply-overlays-dtmerge.enable = true;
-                        raspberry-pi."4".fkms-3d.enable = true; # Enable hardware acceleration
-                        deviceTree = {
-                            enable = true;
-                            filter = "*rpi-4-*.dtb";
-                        };
-                    };
-
-                    # Networking configuration
-                    networking = {
-                        hostname = "rpi";
-                        interfaces."wlan0".useDHCP = true;
-                        wireless = {
-                            enable = true;
-                            interface = [ "wlan0" ];
-                        };
-                    };
-
-                    users.users."rpi" = {
-                        isNormalUser = true;
-                        extraGroups = [ "wheel" ];
-                        hashedPassword = "$y$j9T$rg0syrPPtjaLILPTTplI3/$5uykqP9tXjAsvOocbfosUeN6j6dMrHRUtwudKd4QaA5"; # password generated with `mkpasswd` command
-
-                        # User wide packages
-                        packages = [];
-                    };
-
-                    # System wide packages
-                    environment.systemPackages = with nixpkgs; [
-                        libraspberrypi
-                        raspberrypi-eeprom
-                    ];
-
-                    # Garbage collector
-                    nix.gc = {
-                        automatic = true;
-                        dates = "weekly";
-                        options = "--delete-older-than 7d";
-                    };
-
-                    nix.settings = {
-                        auto-optimise-store = true; # Related to garbage collector
-                        experimental-features = "nix-command flakes"; # Enable flakes
-                    };
-
-                    # "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix" creates a
-                    # disk with this label on first boot. Therefore, we need to keep it. It is the
-                    # only information from the installer image that we need to keep persistent
-                    fileSystems."/" = {
-                        device = "/dev/disk/by-label/NIXOS_SD";
-                        fsType = "ext4";
-                    };  
-
-                    # This value determines the NixOS release from which the default
-                    # settings for stateful data, like file locations and database versions
-                    # on your system were taken. It‘s perfectly fine and recommended to leave
-                    # this value at the release version of the first install of this system.
-                    # Before changing this value read the documentation for this option
-                    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-                    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-                    system.stateVersion = "24.11"; # Did you read the comment?
-                }
-            ];
-            
-        };
-
-        # SD Card image builder
-        images."rpi" = (self.nixosConfiguration."rpi".extendModules {
-            modules = [ 
-                "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-                {
-                    # We don't need this defaults modules
-                    disableModules = ["profiles/base.nix"];
-
-                    # Copy configuration files to system
-                    environment.etc."nixos".source = ./.;
-
-                    # Patch build images on x86-64 systems
-                    nixpkgs.config.allowUnsupportedSystem = true;
-                    nixpkgs.hostPlatform.system = "armv7l-linux";
-                    nixpkgs.buildPlatform.system = "x86_64-linux";   
-                }
-            ];
-        }).config.system.build.sdImage;
-    };
+  };
 }
